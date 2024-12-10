87413886c4f4bb87dec050df9ca8245016c83d85
Ahmad Tsaqif <ahmadtsaqif007@gmail.com>
2024-12-10 22:31:50 +0700

    scenes, music change

---
+++ Assets/Scripts/Audio/PauseMenuMusic.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuMusic : MonoBehaviour
+{
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "Level 1")
+            MenuMusic.instance.GetComponent<AudioSource>().Pause();
+    }
+}
