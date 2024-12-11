3146abfebb2cc6b233843028dca918f32b6c0c97
gilanggnw <gilanggunawansaputra@gmail.com>
2024-12-11 21:22:18 +0700

    Added components to level 2

---
+++ Assets/DialogueTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public NPCConversation TextConversation;
+
+    public void Start()
+    {
+        ConversationManager.Instance.StartConversation(TextConversation);
+    }
+
+}
